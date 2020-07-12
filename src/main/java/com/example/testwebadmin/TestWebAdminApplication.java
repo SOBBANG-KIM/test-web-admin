21102e7871cce1d27bc6d5cc94cbc707647d2963
KSY <kox501@naver.com>
2020-07-12 18:30:25 +0900

    first commit create project

---
+++ src/main/java/com/example/testwebadmin/TestWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwebadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testwebadmin/TestWebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwebadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
